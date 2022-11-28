ea0feafea28da923dc56326aa44e69200f64ca65
Андрей Петров <mrandrushik@gmail.com>
2022-11-28 16:10:59 +0300

    v0.3 main.yml

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,7 +8,7 @@ export function buildPlugins({
     isDev,
     paths,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -20,7 +20,12 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        isDev ? new BundleAnalyzerPlugin({ openAnalyzer: false }) : undefined,
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    }
+
+    return plugins;
 }
