393d3248ac03628ad88cfbfe3b94b9c09a5d7695
Андрей Петров <mrandrushik@gmail.com>
2023-01-04 15:03:29 +0300

    theme addon storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,9 @@
 import { addDecorator } from '@storybook/react';
-import { SuspenseDecorator } from 'shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { RouteDecorator } from 'shared/config/storybook/RouteDecorator/RouteDecorator';
-import { StyleDecorator } from 'shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'shared/const/theme';
+import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { RouteDecorator } from '../../src/shared/config/storybook/RouteDecorator/RouteDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/shared/const/theme';
 
 export const parameters = {
     action: { argTypesRegex: '^on[A-Z].*' },
@@ -14,6 +14,13 @@ export const parameters = {
         },
     },
     layout: 'fullscreen',
+    themes: {
+        default: 'dark',
+        list: [
+            { name: 'dark', class: Theme.DARK, color: '#00aced' },
+            { name: 'light', class: Theme.LIGHT, color: '#3b5998' },
+        ],
+    },
 };
 
 addDecorator(RouteDecorator);
