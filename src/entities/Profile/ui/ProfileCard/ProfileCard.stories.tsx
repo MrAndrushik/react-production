9caa9cb2f257796ceafa623bd63ba2e5f22a33fa
Андрей Петров <mrandrushik@gmail.com>
2023-01-05 14:51:31 +0300

    fix profileCard stories

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -2,16 +2,13 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Country } from '@/entities/Country';
 import { Currency } from '@/entities/Currency';
 import { ProfileCard } from './ProfileCard';
-import avatar from '@/shared/assets/icons/avatar.jpeg';
 
 export default {
     title: 'entities/ProfileCard',
     component: ProfileCard,
 } as ComponentMeta<typeof ProfileCard>;
 
-const Template: ComponentStory<typeof ProfileCard> = (args) => (
-    <ProfileCard {...args} />
-);
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
@@ -23,7 +20,7 @@ Primary.args = {
         currency: Currency.RUB,
         country: Country.Russia,
         username: 'mrandrushik',
-        avatar,
+        avatar: 'https://news.artnet.com/app/news-upload/2021/09/Yuga-Labs-Bored-Ape-Yacht-Club-4466.jpg',
     },
 };
 
